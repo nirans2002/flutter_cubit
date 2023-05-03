19f8d36cb1e26da3bd9a6548e9067896d35c8a2e
Niran S Narayanan <nirans2002@gmail.com>
2023-05-03 21:43:18 +0530

    separate file

--- lib/components/glance_week.dart
+++ lib/components/glance_week.dart
@@ -1,3 +1,4 @@
+import 'package:cubits_task/components/day_streak_widget.dart';
 import 'package:flutter/material.dart';
 
 class WeekGlanceCard extends StatelessWidget {
@@ -61,72 +62,6 @@ class WeekGlanceCard extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class DayStreakWidget extends StatelessWidget {
-  const DayStreakWidget({
-    super.key,
-    required this.streakCount,
-    required this.day,
-  });
-  final int streakCount;
-  final String day;
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SizedBox(
-          width: 40,
-          height: 60,
-          child: Container(
-            decoration: BoxDecoration(
-              color: const Color(0xFFF1F1F1),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    day,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: streakCount == 0
-                            ? const Color(0xFF9A9A9A)
-                            : const Color(0xFF6A67DA),
-                        fontFamily: "Thunder"),
-                  ),
-                  Text(
-                    streakCount == 0 ? "-" : streakCount.toString(),
-                    style: const TextStyle(
-                        fontSize: 14,
-                        color: Color(0xFF6A67DA),
-                        fontFamily: "Thunder"),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          top: 55,
-          left: 10,
-          child: SizedBox(
-            width: 18,
-            height: 8,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: streakCount == 0
-                      ? const Color(0xFFF1F1F1)
-                      : const Color(0xFF6A67DA)),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }

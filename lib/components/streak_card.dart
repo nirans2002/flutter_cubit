df2209ad44b031abae0f2e89621de452f0435a8c
Niran S Narayanan <nirans2002@gmail.com>
2023-05-08 15:01:27 +0530

    calendar

--- lib/components/streak_card.dart
+++ lib/components/streak_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class StreakCard extends StatefulWidget {
   const StreakCard({
@@ -11,6 +12,18 @@ class StreakCard extends StatefulWidget {
 
 class _StreakCardState extends State<StreakCard> {
   bool isExpanded = false;
+  final Map<DateTime, List<dynamic>> _events = {
+    DateTime(2021, 6, 22): [
+      'Meeting URUS',
+      'Testing Danai Mobile',
+      'Weekly Report',
+      'Weekly Meeting'
+    ],
+    DateTime(2021, 6, 25): ['Weekly Testing'],
+    DateTime(2021, 6, 4): ['Weekly Testing'],
+    DateTime(2021, 6, 11): ['Weekly Testing'],
+    DateTime(2021, 6, 18): ['Weekly Testing'],
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +43,7 @@ class _StreakCardState extends State<StreakCard> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: AnimatedContainer(
-              height: isExpanded ? 150 : 45,
+              height: isExpanded ? MediaQuery.of(context).size.height / 2 : 45,
               duration: const Duration(seconds: 2),
               curve: Curves.bounceOut,
               child: Column(
@@ -91,7 +104,103 @@ class _StreakCardState extends State<StreakCard> {
                     ],
                   ),
                   isExpanded
-                      ? const Text("calender")
+                      ? TableCalendar(
+                          firstDay: DateTime.utc(2010, 10, 16),
+                          lastDay: DateTime.utc(2030, 3, 14),
+                          focusedDay: DateTime.now(),
+                          startingDayOfWeek: StartingDayOfWeek.monday,
+                          calendarBuilders: CalendarBuilders(
+                            // markerBuilder: (context, day, events) => ,
+                            singleMarkerBuilder: (context, day, event) {
+                              return Center(
+                                child: Stack(
+                                  children: const [
+                                    Text(
+                                      "S",
+                                      style: TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 125, 125, 125),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                            dowBuilder: (context, day) {
+                              if (day.weekday == DateTime.sunday) {
+                                return const Center(
+                                  child: Text(
+                                    "S",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 125, 125, 125),
+                                    ),
+                                  ),
+                                );
+                              }
+                              if (day.weekday == DateTime.monday) {
+                                return const Center(
+                                  child: Text(
+                                    "M",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 125, 125, 125),
+                                    ),
+                                  ),
+                                );
+                              }
+                              if (day.weekday == DateTime.tuesday) {
+                                return const Center(
+                                  child: Text(
+                                    "T",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 125, 125, 125),
+                                    ),
+                                  ),
+                                );
+                              }
+                              if (day.weekday == DateTime.wednesday) {
+                                return const Center(
+                                  child: Text(
+                                    "W",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 125, 125, 125),
+                                    ),
+                                  ),
+                                );
+                              }
+                              if (day.weekday == DateTime.thursday) {
+                                return const Center(
+                                  child: Text(
+                                    "T",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 125, 125, 125),
+                                    ),
+                                  ),
+                                );
+                              }
+                              if (day.weekday == DateTime.friday) {
+                                return const Center(
+                                  child: Text(
+                                    "F",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 125, 125, 125),
+                                    ),
+                                  ),
+                                );
+                              }
+                              if (day.weekday == DateTime.saturday) {
+                                return const Center(
+                                  child: Text(
+                                    "S",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 125, 125, 125),
+                                    ),
+                                  ),
+                                );
+                              }
+                              return null;
+                            },
+                          ),
+                        )
                       : const SizedBox(
                           height: 0,
                         )
